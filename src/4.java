f46d797478b6648e7ccf85c042166cbc3a44b704
shantellbourret794 <shantellbourret794@gmail.com>
2025-03-09 12:43:51 +0000

    Add 4.java

---
+++ src/4.java
@@ -0,0 +1,6 @@
+public class RandomJavaCode {
+    public static void main(String[] args) {
+        int num = (int)(Math.random() * 10);
+        System.out.println("Random number between 0 and 9: " + num);
+    }
+}
