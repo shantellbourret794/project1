c75bfb745bb64471ada1e350d3104052621b882a
shantellbourret794 <shantellbourret794@gmail.com>
2025-03-17 23:49:23 +0000

    Add 12.java

---
+++ src/12.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class Project1 {
+    public static void main(String[] args) {
+        int number = new Random().nextInt(10);
+        System.out.println("The random number is: " + number);
+    }
+}
