276fd1272636032dbe5c813e43ddd4c4782c24a1
shantellbourret794 <shantellbourret794@gmail.com>
2025-04-23 10:37:08 +0000

    Add 31.java

---
+++ src/31.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        // Add more code here if needed
+    }
+}
